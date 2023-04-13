428c201d3eae4be1bac926e53d73e828cd4ca3f2
Tumbo Lobi John <johnlobiit@gmail.com>
2023-04-13 11:20:05 +0300

    created the generate new id functionality, the chat stripe markup and handling submit

--- script.js
+++ script.js
@@ -30,3 +30,55 @@ function typeText(element, text) {
         }
     }, 10)
 }
+
+function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe (isAi, value, uniqueId) {
+    return (
+        `
+            <div class="wrapper ${isAi && 'ai'}">
+                <div class="chat" ">
+                    <div className="profile"> 
+                        <img src="${isAi ? bot : user}"  alt="${isAi ? 'bot' : 'user' }"/>
+                    </div>
+                    <div class="message" id="${uniqueId}">${value}</div>
+                </div>                
+             </div>
+        `
+    )
+}
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    // users chatstripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+    form.reset();
+
+    // bot's chatstripe
+    const uniqueId = generateUniqueId();
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    const messageDiv = document.getElementById(uniqueId);
+
+    loader(messageDiv);
+}
+
+form.addEventListener('submit', handleSubmit);
+
+form.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        handleSubmit(e);
+    }
+})
